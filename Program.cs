e364a3491dcf2bd63fa510fc64350690da785842
rubayetseason <rubayetislamseason5@gmail.com>
2026-05-10 16:14:45 +0600

    variables and data types

--- Program.cs
+++ Program.cs
@@ -4,9 +4,32 @@ class Test
     public static void Main(string[] args)
     {
         // number
-        Console.WriteLine(123);
+        // Console.WriteLine(123);
         // string
-        Console.Write("This sentence is printed with a newline.\n");
-        Console.WriteLine("Hello World from Season!");
+        // Console.Write("This sentence is printed with a newline.\n");
+        // Console.WriteLine("Hello World from Season!");
+
+        // variables
+        /*
+        int --> 112, -112
+        double --> 1.1, -1.1
+        float --> 1.1f, -1.1f
+        bool --> true, false
+        char --> 'a', 'A'
+        string --> "hello world"
+        */
+
+        string name = "John";
+        int age = 20;
+        bool isMale = true;
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(isMale);
+
+        /*
+        output --> John, 20, True
+        */
+
+
     }
 }
