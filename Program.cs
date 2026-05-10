04d14f3c68ec135c8baf32aae759497feb4df2d4
rubayetseason <rubayetislamseason5@gmail.com>
2026-05-10 16:09:27 +0600

    write command usage

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿class Test
+﻿using System;
+class Test
 {
     public static void Main(string[] args)
     {
+        // number
+        Console.WriteLine(123);
+        // string
+        Console.Write("This sentence is printed with a newline.\n");
         Console.WriteLine("Hello World from Season!");
     }
 }
